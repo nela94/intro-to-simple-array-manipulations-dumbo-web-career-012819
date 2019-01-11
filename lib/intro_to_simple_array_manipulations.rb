049f98bb9f0c51af1f112636acec44d72a7e0202
Manuela  Sanchez <m.sanchez947@hotmail.com>
2019-01-11 13:48:29 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -32,6 +32,6 @@ end
 def using_flatten(private_colleges_in_newyork)
   private_colleges_in_newyork.flatten
 end
-def using_delete(instructors)
-    instructors.delete("Steven")
+def using_delete(instructors, "Steven")
+    no_offense_steven = instructors.delete("Steven")
 end
